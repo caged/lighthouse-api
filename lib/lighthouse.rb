ed4452a2a8f654b59bbf875031c5f06d060b3eb9
rick <technoweenie@gmail.com>
2007-04-29 03:30:30 +0000

    simplify account/token setting
    git-svn-id: http://ar-code.svn.engineyard.com/lighthouse-api@7 d98f8484-0aa7-43af-80ea-302c351499d6

--- lib/lighthouse.rb
+++ lib/lighthouse.rb
@@ -4,56 +4,44 @@ require 'active_support'
 require 'active_resource'
 
 module Lighthouse
+  class << self
+    attr_reader :account, :token
+
+    def resources
+      @resources ||= []
+    end
+
+    def account=(name)
+      resources.each do |klass|
+        klass.site = klass.site_format % name
+      end
+      @account = name
+    end
+
+    def token=(value)
+      resources.each do |klass|
+        klass.custom_headers['X-LighthouseToken'] = value
+      end
+      @token = value
+    end
+  end
+
   class Base < ActiveResource::Base
     def self.inherited(base)
-      (self.resources ||= []) << base
+      Lighthouse.resources << base
       class << base
-        def account
-          Lighthouse::Base.account
-        end
-
-        def account=(name)
-          Lighthouse::Base.account = name
-        end
-
-        def token
-          Lighthouse::Base.token
-        end
-
-        def token=(value)
-          Lighthouse::Base.token = value
-        end
-        
         attr_accessor :site_format
       end
       base.site_format = 'http://%s.lighthouseapp.com'
       super
     end
     
-    class << self
-      attr_accessor :resources, :account, :token
-
-      def account=(name)
-        resources.each do |klass|
-          klass.site = klass.site_format % name
-        end
-        @account = name
-      end
-
-      def token=(value)
-        resources.each do |klass|
-          klass.custom_headers['X-LighthouseToken'] = value
-        end
-        @token = value
-      end
-    end
-    
     def account
-      self.class.account
+      Lighthouse.account
     end
     
     def token
-      self.class.token
+      Lighthouse.token
     end
   end
   
