c5de6b00483fc617e01489fe168a9a05a5bc94ac
rick <technoweenie@gmail.com>
2007-08-07 04:46:05 +0000

    add console support, as well as ticket tagging, users, memberships, and tokens
    git-svn-id: http://ar-code.svn.engineyard.com/lighthouse-api@19 d98f8484-0aa7-43af-80ea-302c351499d6

--- lib/lighthouse.rb
+++ lib/lighthouse.rb
@@ -25,14 +25,15 @@ require 'active_resource'
 # http://lighthouseapp.com/api.
 #
 module Lighthouse
+  class Error < StandardError; end
   class << self
-    attr_accessor :email, :password
+    attr_accessor :email, :password, :host_format, :domain_format, :protocol, :port
     attr_reader :account, :token
 
     # Sets the account name, and updates all the resources with the new domain.
     def account=(name)
       resources.each do |klass|
-        klass.site = klass.site_format % name
+        klass.site = klass.site_format % (host_format % [protocol, domain_format % name, port])
       end
       @account = name
     end
@@ -55,6 +56,11 @@ module Lighthouse
       @resources ||= []
     end
   end
+  
+  self.host_format   = '%s://%s%s'
+  self.domain_format = '%s.lighthouseapp.com'
+  self.protocol      = 'http'
+  self.port          = ''
 
   class Base < ActiveResource::Base
     def self.inherited(base)
@@ -62,7 +68,7 @@ module Lighthouse
       class << base
         attr_accessor :site_format
       end
-      base.site_format = 'http://%s.lighthouseapp.com'
+      base.site_format = '%s'
       super
     end
   end
@@ -90,7 +96,7 @@ module Lighthouse
   #   project = Project.find(44)
   #   project.tickets
   #
-  class Project < Lighthouse::Base
+  class Project < Base
     def tickets(options = {})
       Ticket.find(:all, :params => options.update(:project_id => id))
     end
@@ -105,6 +111,25 @@ module Lighthouse
   
     def bins(options = {})
       Bin.find(:all, :params => options.update(:project_id => id))
+    end
+  end
+
+  class User < Base
+    def memberships
+      Membership.find(:all, :params => {:user_id => id})
+    end
+  end
+  
+  class Membership < Base
+    site_format << '/users/:user_id'
+    def save
+      raise Error, "Cannot modify Memberships from the API"
+    end
+  end
+  
+  class Token < Base
+    def save
+      raise Error, "Cannot modify Tokens from the API"
     end
   end
 
@@ -130,19 +155,69 @@ module Lighthouse
   #  ticket.state = 'resolved'
   #  ticket.save
   #
-  class Ticket < Lighthouse::Base
+  class Ticket < Base
+    attr_writer :tags
+    site_format << '/projects/:project_id'
+
+    def id
+      attributes['number'] ||= nil
+      number
+    end
+
+    def tags
+      attributes['tag'] ||= nil
+      @tags ||= tag.blank? ? [] : parse_with_spaces(tag)
+    end
+
+    def save_with_tags
+      self.tag = @tags.collect do |tag|
+        tag.include?(' ') ? tag.inspect : tag
+      end.join(" ") if @tags.is_a?(Array)
+      @tags = nil ; save_without_tags
+    end
+    
+    alias_method_chain :save, :tags
+
+    private
+      # taken from Lighthouse Tag code
+      def parse_with_spaces(list)
+        tags = []
+
+        # first, pull out the quoted tags
+        list.gsub!(/\"(.*?)\"\s*/ ) { tags << $1; "" }
+        
+        # then, get whatever's left
+        tags.concat list.split(/\s/)
+
+        cleanup_tags(tags)
+      end
+    
+      def cleanup_tags(tags)
+        returning tags do |tag|
+          tag.collect! do |t|
+            unless tag.blank?
+              t.downcase!
+              t.gsub! /(^')|('$)/, ''
+              t.gsub! /[^a-z0-9 \-_@\!']/, ''
+              t.strip!
+              t
+            end
+          end
+          tag.compact!
+          tag.uniq!
+        end
+      end
+  end
+  
+  class Message < Base
     site_format << '/projects/:project_id'
   end
   
-  class Message < Lighthouse::Base
+  class Milestone < Base
     site_format << '/projects/:project_id'
   end
   
-  class Milestone < Lighthouse::Base
-    site_format << '/projects/:project_id'
-  end
-  
-  class Bin < Lighthouse::Base
+  class Bin < Base
     site_format << '/projects/:project_id'
   end
 end
