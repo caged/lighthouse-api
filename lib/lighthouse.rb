9b5c52d64e4e3333ad5eb1a49580a77ad840abd8
Grant Hollingworth <grant@antiflux.org>
2008-11-25 20:26:59 -0500

    added tickets method to Bin

--- lib/lighthouse.rb
+++ lib/lighthouse.rb
@@ -273,6 +273,10 @@ module Lighthouse
   
   class Bin < Base
     site_format << '/projects/:project_id'
+
+    def tickets(options = {})
+      Ticket.find(:all, :params => options.merge(prefix_options).update(:q => query))
+    end
   end
   
   class Changeset < Base
