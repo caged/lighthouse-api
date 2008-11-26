b947c6695be7892c7d585333308c961154c2ed67
Grant Hollingworth <grant@antiflux.org>
2008-11-25 20:35:04 -0500

    added tickets method to Milestone

--- lib/lighthouse.rb
+++ lib/lighthouse.rb
@@ -269,6 +269,10 @@ module Lighthouse
   
   class Milestone < Base
     site_format << '/projects/:project_id'
+
+    def tickets(options = {})
+      Ticket.find(:all, :params => options.merge(prefix_options).update(:q => "milestone:'#{title}'"))
+    end
   end
   
   class Bin < Base
